e1d5cc4c6e8eff96f57de93efcac10e3b1da7703
ignatiusobiorah <iobiorah23@gmail.com>
2022-09-05 22:16:42 +0100

    added registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,19 @@ import React, { Fragment } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/custom.scss";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Training from "./pages/Training";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import Header from "./components/shared/Header";
-import Footer from "./components/shared/Footer";
-
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Training from "./pages/Training";
+// import Services from "./pages/Services";
+// import Contact from "./pages/Contact";
+// import Header from "./components/shared/Header";
+// import Footer from "./components/shared/Footer";
+import Register from "./components/Register";
 const App: React.FC = () => {
 	return (
 		<Fragment>
-			<Header />
+			{/* <Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
@@ -22,7 +22,8 @@ const App: React.FC = () => {
 				<Route path="/services" element={<Services />} />
 				<Route path="/contact" element={<Contact />} />
 			</Routes>
-			<Footer />
+			<Footer /> */}
+			<Register />
 		</Fragment>
 	);
 };
---
+++ frontend/src/components/Register.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "../styles/training-page.scss";
+
+const Register: React.FC = () => {
+	return (
+		<div className="register">
+			<div className="registerContainer">
+				<div className="imgContainer">
+					<img src="/images/train-bg.png" alt="train.png" />
+				</div>
+
+				<div className="registerContent">
+					<h1 className="formH1">Register</h1>
+					<form className="registerForm">
+						<label htmlFor="firstName">Firstname:</label>
+						<input type="text" name="firstname" id="firstname" required />
+						<label htmlFor="lastName">Lastname:</label>
+						<input type="text" name="lastname" id="lastname" required />
+						<label htmlFor="selectCourse">Select Course:</label>
+						<select name="course" id="course">
+							<option value="">Select course</option>
+							<option value="frontEnd">Frontend</option>
+							<option value="backEnd">Backend</option>
+							<option value="fullStack">Fullstack</option>
+						</select>
+						<label htmlFor="phoneNumber">Phone:</label>
+						<input type="number" name="phonenumber" id="phoneNumber" required />
+						<label htmlFor="email">Email:</label>
+						<input type="text" name="email" id="email" required />
+						<label htmlFor="gender">Gender:</label>
+						<input type="text" name="gender" id="gender" required />
+
+						<hr />
+
+						<p>
+							Clicking submit you agree to <a href="/">Terms & Privacy.</a>
+						</p>
+
+						<button>Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
