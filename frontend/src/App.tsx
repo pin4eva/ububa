41e1c823b0829a7fde58d789fd3eda78be3d1acb
ignatiusobiorah <iobiorah23@gmail.com>
2022-09-06 12:08:59 +0100

    added front layout component to training page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/custom.scss";
-<<<<<<< HEAD
 // import { Route, Routes } from "react-router-dom";
 // import Home from "./pages/Home";
 // import About from "./pages/About";
@@ -16,24 +15,12 @@ const App: React.FC = () => {
 	return (
 		<Fragment>
 			{/* <Header />
-=======
-import { Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Training from "./pages/Training";
-
-const App: React.FC = () => {
-	return (
-		<Fragment>
->>>>>>> 9553598d189c348d1cd24be55926d2edc6e1c6cf
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
 				<Route path="/training" element={<Training />} />
 			</Routes>
-<<<<<<< HEAD
 			<Footer /> */}
 			<Register />
-=======
->>>>>>> 9553598d189c348d1cd24be55926d2edc6e1c6cf
 		</Fragment>
 	);
 };
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,45 +1,55 @@
 import React from "react";
 import "../styles/training-page.scss";
+import FrontLayout from "./shared/FrontLayout";
 
 const Register: React.FC = () => {
 	return (
 		<div className="register">
-			<div className="registerContainer">
-				<div className="imgContainer">
-					<img src="/images/train-bg.png" alt="train.png" />
+			<FrontLayout showHeader={true}>
+				<div className="registerContainer">
+					<div className="imgContainer">
+						<img src="/images/train-bg.png" alt="train.png" />
+					</div>
+
+					<div className="registerContent">
+						<h1 className="formH1">Register</h1>
+						<form className="registerForm">
+							<label htmlFor="firstName">Firstname:</label>
+							<input type="text" name="firstname" id="firstname" required />
+							<label htmlFor="lastName">Lastname:</label>
+							<input type="text" name="lastname" id="lastname" required />
+							<label htmlFor="selectCourse">Select Course:</label>
+							<select name="course" id="course">
+								<option value="">Select course</option>
+								<option value="frontEnd">Mongo DB</option>
+								<option value="backEnd">Javascript</option>
+								<option value="fullStack">React</option>
+								<option value="fullStack">React Native Dev.</option>
+								<option value="fullStack">Android Dev.</option>
+							</select>
+							<label htmlFor="phoneNumber">Phone:</label>
+							<input
+								type="number"
+								name="phonenumber"
+								id="phoneNumber"
+								required
+							/>
+							<label htmlFor="email">Email:</label>
+							<input type="text" name="email" id="email" required />
+							<label htmlFor="gender">Gender:</label>
+							<input type="text" name="gender" id="gender" required />
+
+							<hr />
+
+							<p>
+								Clicking submit you agree to <a href="/">Terms & Privacy.</a>
+							</p>
+
+							<button>Submit</button>
+						</form>
+					</div>
 				</div>
-
-				<div className="registerContent">
-					<h1 className="formH1">Register</h1>
-					<form className="registerForm">
-						<label htmlFor="firstName">Firstname:</label>
-						<input type="text" name="firstname" id="firstname" required />
-						<label htmlFor="lastName">Lastname:</label>
-						<input type="text" name="lastname" id="lastname" required />
-						<label htmlFor="selectCourse">Select Course:</label>
-						<select name="course" id="course">
-							<option value="">Select course</option>
-							<option value="frontEnd">Frontend</option>
-							<option value="backEnd">Backend</option>
-							<option value="fullStack">Fullstack</option>
-						</select>
-						<label htmlFor="phoneNumber">Phone:</label>
-						<input type="number" name="phonenumber" id="phoneNumber" required />
-						<label htmlFor="email">Email:</label>
-						<input type="text" name="email" id="email" required />
-						<label htmlFor="gender">Gender:</label>
-						<input type="text" name="gender" id="gender" required />
-
-						<hr />
-
-						<p>
-							Clicking submit you agree to <a href="/">Terms & Privacy.</a>
-						</p>
-
-						<button>Submit</button>
-					</form>
-				</div>
-			</div>
+			</FrontLayout>
 		</div>
 	);
 };
