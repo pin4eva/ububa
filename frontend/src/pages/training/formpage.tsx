5cf56408d6a10a00ea14d9e2c3c1d116085dad51
ignatiusobiorah <iobiorah23@gmail.com>
2022-09-11 20:24:36 +0100

    added trainging page index and registration page

---
+++ frontend/src/pages/training/formpage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FrontLayout from "../../components/shared/FrontLayout";
+import FormPage from "../../components/FormPage";
+
+const formpage = () => {
+	return (
+		<FrontLayout>
+			<FormPage />
+		</FrontLayout>
+	);
+};
+
+export default formpage;
